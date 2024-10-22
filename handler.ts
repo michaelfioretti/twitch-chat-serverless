adce69a310f675945538fd300cb24834dee4fc37
Michael Fioretti <michaelfioretti1@gmail.com>
2024-10-21 18:42:50 -0700

    Update .gitignore; add /search endpoint

--- handler.ts
+++ handler.ts
@@ -10,4 +10,17 @@ app.get("/livestreams", async (_req: Request, res: Response) => {
   res.json({ results })
 })
 
+app.post('/search', async (req: Request, res: Response) => {
+  if (!req.body.query) {
+    res.status(400).send({
+      message: 'Missing body parameter "query"'
+    })
+
+    return
+  }
+
+  const results = await twitchManager.SearchForTwitchChannel(req.body.query)
+  res.json({ results })
+})
+
 export const handler = serverless(app);
