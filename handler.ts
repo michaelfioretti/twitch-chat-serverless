8f07086cbf381a818a721820e52ab5595ec09677
Michael Fioretti <michaelfioretti1@gmail.com>
2024-10-22 16:47:58 -0700

    Install helmet and use helmet/express json middleware; update search to return TwitchStream[]

--- handler.ts
+++ handler.ts
@@ -1,9 +1,13 @@
 import serverless from "serverless-http";
 import express, { Request, Response } from "express";
+import helmet from 'helmet'
 import TwitchManager from "./managers/twitch";
 
 const app = express();
 const twitchManager = new TwitchManager()
+
+app.use(express.json())
+app.use(helmet())
 
 app.get("/livestreams", async (_req: Request, res: Response) => {
   const results = await twitchManager.GetTopTwitchLiveStreams()
@@ -11,6 +15,7 @@ app.get("/livestreams", async (_req: Request, res: Response) => {
 })
 
 app.post('/search', async (req: Request, res: Response) => {
+  console.log('here is the req boduy: ', req.body)
   if (!req.body.query) {
     res.status(400).send({
       message: 'Missing body parameter "query"'
--- managers/twitch.ts
+++ managers/twitch.ts
@@ -42,11 +42,15 @@ class TwitchManager {
     return combineStreamerAndStreamData(streams, streamers)
   }
 
-  async SearchForTwitchChannel(query: string): Promise<TwitchChannel[]> {
+  async SearchForTwitchChannel(query: string): Promise<TwitchStream[]> {
     if (!this.oauthToken) {
       await this.GetTwitchToken()
     }
 
+    // The search below will return an array of TwitchChannel types. After this, we
+    // need to get the associated streamer information like when we fetch the top
+    // 100 livestreams
+    console.log('here is the body that we got: ', query)
     try {
       const response = await axios.get(TWITCH_SEARCH_URL, {
         headers: {
@@ -54,11 +58,25 @@ class TwitchManager {
           Authorization: `Bearer ${this.oauthToken}`,
         },
         params: {
-          query: query,
+          query
         },
       });
 
-      return response.data.data;
+      // Now we need to associated the specific Twitch streamers to their
+      // livestreams that are currently active
+      const streams = response.data.data
+      const userIds = streams.map((stream: TwitchStream) => stream.user_id)
+      const streamersResponse = await axios.get(TWITCH_USERS_URL, {
+        params: { id: userIds },
+        headers: {
+          'Client-ID': process.env.TWITCH_CLIENT_ID,
+          Authorization: `Bearer ${this.oauthToken}`,
+        },
+      });
+
+      const streamers = streamersResponse.data.data
+
+      return combineStreamerAndStreamData(streams, streamers)
     } catch (error) {
       throw error;
     }
