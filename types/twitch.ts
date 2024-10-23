082880f7dd8885c45f5ac88f1042bd0e778c07ea
Michael Fioretti <michaelfioretti1@gmail.com>
2024-10-23 09:48:07 -0700

    Remove duplicate types

--- types/twitch.ts
+++ types/twitch.ts
@@ -39,38 +39,3 @@ export interface TwitchUser {
   profile_image_url: string;
   description: string;
 }
-
-export interface TwitchStream {
-  game_id: string,
-  game_name: string,
-  id: string,
-  is_mature: boolean,
-  language: string,
-  profileImage: string | undefined,
-  started_at: string,
-  streamerName: string | undefined,
-  tag_ids: string[],
-  tags: string[],
-  thumbnail_url: string,
-  title: string,
-  type: string,
-  user_id: string,
-  user_login: string,
-  user_name: string,
-  viewer_count: number
-}
-
-export interface TwitchChannel {
-  broadcaster_language: string,
-  broadcaster_login: string,
-  display_name: string,
-  game_id: string,
-  game_name: string,
-  id: string,
-  is_live: boolean,
-  tag_ids: string[],
-  tags: string[],
-  thumbnail_url: string,
-  title: string,
-  started_at: string,
-}
